a5980f0b154b87f13aab13c3816a6b92ef646161
Gentry Riggen <gentry.riggen@gmail.com>
2026-03-01 22:23:04 -0700

    fix(ios): guard glassEffect behind #if compiler(>=6.1) for Xcode 16 CI

--- apps/ios/ChapterCheck/Features/Player/NowPlayingView.swift
+++ apps/ios/ChapterCheck/Features/Player/NowPlayingView.swift
@@ -209,6 +209,7 @@ struct NowPlayingView: View {
 
 private struct GlassCircleModifier: ViewModifier {
     func body(content: Content) -> some View {
+        #if compiler(>=6.1)
         if #available(iOS 26.0, *) {
             content
                 .glassEffect(.regular, in: .circle)
@@ -216,11 +217,16 @@ private struct GlassCircleModifier: ViewModifier {
             content
                 .background(.ultraThinMaterial, in: Circle())
         }
+        #else
+        content
+            .background(.ultraThinMaterial, in: Circle())
+        #endif
     }
 }
 
 private struct GlassCapsuleModifier: ViewModifier {
     func body(content: Content) -> some View {
+        #if compiler(>=6.1)
         if #available(iOS 26.0, *) {
             content
                 .glassEffect(.regular, in: .capsule)
@@ -228,5 +234,9 @@ private struct GlassCapsuleModifier: ViewModifier {
             content
                 .background(.ultraThinMaterial, in: Capsule())
         }
+        #else
+        content
+            .background(.ultraThinMaterial, in: Capsule())
+        #endif
     }
 }
